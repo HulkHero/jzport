ca0146931a44958f75a8fbb97c344d02ac032e44
Hammad Zafar <2019ee328@student.uet.edu.pk>
2022-09-25 19:43:31 +0500

    helli

--- src/App.js
+++ src/App.js
@@ -3,12 +3,8 @@ import './App.css';
 import Header from './components/Headers'
 import theme from "./components/theme"
 import {CssBaseline, ThemeProvider} from "@mui/material"
-
 import {BrowserRouter as Router} from 'react-router-dom'
-
 import Contact from './components/Contact/Contact'
-
-
 function App() {
   return (
    <>
--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -2,6 +2,39 @@ import React, { useState ,useRef} from 'react'
 import { Button, TextField,Grid } from '@mui/material'
 import theme from '../theme' 
 import emailjs from '@emailjs/browser';
+import {styled } from '@mui/material/styles';
+
+const CssTextField = styled(TextField)({
+  maxWidth:"600px",
+   ".MuiFormLabel-root":{
+    textColor:"white",
+    color:"white",
+   },
+
+   "& .MuiInputBase-input":{
+    color:"white",
+   },
+
+  '& label.Mui-focused': {
+    color: 'white',
+   
+  },
+  '& .MuiInput-underline:after': {
+    borderBottomColor: 'white',
+  },
+  '& .MuiOutlinedInput-root': {
+    '& fieldset': {
+      borderColor: '#D73F36',
+    },
+    '&:hover fieldset': {
+      borderColor: 'yellow',
+    },
+    '&.Mui-focused fieldset': {
+      borderColor: 'white',
+     
+    },
+  },
+});
 
 const ContactForm = () => {
   const form = useRef();
@@ -38,9 +71,10 @@ const ContactForm = () => {
    
      <form ref={form} onSubmit={sendEmail}>
       <Grid container justifyContent="center"  sx={{width:"90%"}} flexDirection={"column"} >
-    <TextField type="name" value={user.name} name="name" id="outlined-basic"  onChange={handleChange}  label="Name" variant="outlined"  sx={{alignSelf:"center",maxWidth:"600px",mb:'10px',input :{color:`${theme.colors.base2}`,borderColor:`${theme.colors.base2}`,}}} />     
-    <TextField type="email" value={user.email} name="email" id="outlined-basic" onChange={handleChange} sx={{alignSelf:"center",maxWidth:"600px",mb:'10px',input :{color:`${theme.colors.base2}`,borderColor:`${theme.colors.base2}`,}}}  label="Email" variant="outlined" />
-    <TextField type="messege" value={user.messege} label="Details" name="messege" id="outlined-basic" onChange={handleChange} multiline={true} sx={{alignSelf:"center",maxWidth:"600px",mb:'10px',input :{color:`${theme.colors.base2}`,borderColor:`${theme.colors.base2}`,}}} variant="outlined" />
+    <CssTextField type="name" value={user.name} name="name" id="outlined-basic"  onChange={handleChange}  label="Name" variant="outlined"  sx={{alignSelf:"center",maxWidth:"600px",mb:'10px',input :{color:`${theme.colors.base2}`,borderColor:`${theme.colors.base2}`,}}} />     
+    <CssTextField type="email" value={user.email} name="email" id="outlined-basic" onChange={handleChange} sx={{alignSelf:"center",maxWidth:"600px",mb:'10px',input :{color:`${theme.colors.base2}`,borderColor:`${theme.colors.base2}`,}}}  label="Email" variant="outlined" />
+    <CssTextField type="messege" value={user.messege} label="Details" name="messege" id="outlined-basic" onChange={handleChange} multiline={true} sx={{alignSelf:"center",minWidth:"210px",maxWidth:"600px",mb:'10px',input :{color:`${theme.colors.base2}`,borderColor:`${theme.colors.base2}`,}}} variant="outlined" />
+   
     <Button type="submit" variant="contained" value="Send" sx={{alignSelf:"center",width:"50px",bgcolor:"#D73F36",color:"#fffff"}}>{text}</Button>
     </Grid>
     </form>
